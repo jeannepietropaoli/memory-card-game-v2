37e8cfeaff7c7fd91f0b42af025c402559a058fa
Jeanne <j.pietropaoli@yahoo.fr>
2024-02-20 04:38:50 +0100

    Create shuffleArray logic

--- src/Components/CardList.jsx
+++ src/Components/CardList.jsx
@@ -1,11 +1,32 @@
 import Card from './Card';
+import '../style/CardList.css';
 
 export default function CardList(props) {
-    const { data } = props;
+    const { data, selectCard } = props;
+
+    const shuffleArray = (array) => {
+        const arrayCopy = array;
+        const newArray = [];
+        while(arrayCopy.length>0) {
+            let randomIndex = Math.floor(Math.random() * array.length);
+            newArray.push(arrayCopy.splice(randomIndex, 1)[0]);
+        }
+        console.log(newArray);
+        return newArray;
+    }
+
     return (
         <ul className='cards'>
           {data ?
-            data.map(pokemon => <Card key={pokemon.id} pokemon={pokemon} />)
+            shuffleArray(data.map(pokemon => {
+                return (
+                    <Card
+                        selectCard={selectCard}
+                        key={pokemon.id}
+                        pokemon={pokemon}
+                    />
+                )
+            }))
             : <li>Loading...</li>
           }
         </ul>
